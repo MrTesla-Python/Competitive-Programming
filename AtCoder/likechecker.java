16e87fe7002418a49bb2aed2d7986a2584f7a204
MrTesla-Python <63614313+MrTesla-Python@users.noreply.github.com>
2023-09-27 06:43:41 -0400

    coding

---
+++ AtCoder/likechecker.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class likechecker
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        int prev = -5;
+        boolean check = true;
+        while (n >= 10)
+        {
+            int curr = n % 10;
+            if (curr <= prev)
+            {
+                System.out.println("No");
+                check = false;
+                break;
+            }
+            prev = curr;
+            n /= 10;
+        }
+        if (check == true) System.out.println("Yes");
+        in.close();
+    }
+}
